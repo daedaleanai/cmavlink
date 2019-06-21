07e1b840f0bf9988a1c08974502ef3adb9921762
Luuk van Dijk <lvd@daedalean.ai>
2019-06-21 19:09:10 +0200

    updated update.sh

--- update.sh
+++ update.sh
@@ -9,11 +9,11 @@ mkdir -p src
 for ddef in ${XMLDIR}*.xml; do
 	dialect=$(basename -s .xml $ddef | tr '[A-Z]' '[a-z]')
 	echo $dialect ...
-	gomavgen h.tmpl     $ddef | clang-format > include/${dialect}.h
-	gomavgen c_crc.tmpl $ddef | clang-format > src/${dialect}_crc.c
-	gomavgen c_dec.tmpl $ddef | clang-format > src/${dialect}_dec.c
-	gomavgen c_enc.tmpl $ddef | clang-format > src/${dialect}_enc.c
-	gomavgen c_fmt.tmpl $ddef | clang-format > src/${dialect}_fmt.c
+	gomavgen ../gomavgen/h.tmpl     $ddef | clang-format > include/${dialect}.h
+	gomavgen ../gomavgen/c_crc.tmpl $ddef | clang-format > src/${dialect}_crc.c
+	gomavgen ../gomavgen/c_dec.tmpl $ddef | clang-format > src/${dialect}_dec.c
+	gomavgen ../gomavgen/c_enc.tmpl $ddef | clang-format > src/${dialect}_enc.c
+	gomavgen ../gomavgen/c_fmt.tmpl $ddef | clang-format > src/${dialect}_fmt.c
 	(cd src; gcc -I../include -O3 -c ${dialect}_*.c)
 	echo
 done
